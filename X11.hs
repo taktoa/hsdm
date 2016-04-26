2c42397cba10b63c51bfa7843a88f16d2858093b
Remy Goldschmidt <taktoa@gmail.com>
2016-04-26 05:45:01 -0500

    Save before sleep

--- X11.hs
+++ X11.hs
@@ -1,27 +1,37 @@
+{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module X11 where
+module Main where
 
-import qualified Codec.Picture.Png        as JP
-import qualified Codec.Picture.Types      as JP
+import qualified Codec.Picture.Png           as JP
+import qualified Codec.Picture.Types         as JP
 import           Control.Concurrent
 import           Control.Monad
 import           Control.Monad.Reader
 import           Data.Array.Storable
 import           Data.Bits
-import           Data.ByteString          (ByteString, useAsCString)
-import qualified Data.ByteString          as BS
+import           Data.ByteString             (ByteString, useAsCString)
+import qualified Data.ByteString             as BS
+import qualified Data.Colour.Names           as C
 import           Data.Default
 import           Data.Int
 import           Data.Maybe
+import           Data.Semigroup
+import           Data.Typeable               (Typeable)
 import           Data.Word
-import           Foreign                  (castPtr)
-import           Foreign.C.String         (CString)
-import qualified Graphics.X11.Xlib        as X
-import qualified Graphics.X11.Xlib.Extras as X
+import           Diagrams
+import           Diagrams.Backend.Rasterific
+import           Foreign                     (castPtr)
+import           Foreign.C.String            (CString)
+import           Graphics.Rasterific.Svg
+import qualified Graphics.X11.Xlib           as X
+import qualified Graphics.X11.Xlib.Extras    as X
 import           System.Exit
 import           System.IO
 import           System.Random
+
+main :: IO ()
+main = testDrawImage
 
 type Pos = X.Position
 type Dim = X.Dimension
@@ -38,21 +48,128 @@ data XImg = XImg { xImage     :: X.Image
                  , xImageW    :: Int
                  }
 
-testDrawImage :: FilePath -> IO ()
-testDrawImage file = do
-  edimg <- JP.decodePng <$> BS.readFile file
-  case edimg of
-    Left  err  -> putStrLn err
-    Right dimg -> do
-      dpy <- X.openDisplay ""
-      win <- mkUnmanagedWindow dpy 1000 400 500 500
-      X.selectInput dpy win X.exposureMask
-      X.mapWindow dpy win
-      fromMaybe
-        (putStrLn "FAILURE")
-        (drawJPImage dpy win <$> convertDynImage dimg)
-      _ <- getLine
-      X.closeDisplay dpy
+type XDiagram = QDiagram Rasterific V2 Double Any
+
+drawDia :: Display -> Window -> XDiagram -> IO ()
+drawDia dpy win = drawJPImage dpy win
+                  . renderDia Rasterific options
+  where
+    options = RasterificOptions (mkWidth 500)
+
+example1, example2 :: XDiagram
+example1 = fc C.blue (circle 3) ||| fc C.red  (rect 5 8)
+example2 = fc C.red  (circle 3) ||| fc C.blue (rect 5 8)
+data X11EventType = EKeyPress
+                  | EKeyRelease
+                  | EButtonPress
+                  | EButtonRelease
+                  | EMotionNotify
+                  | EEnterNotify
+                  | ELeaveNotify
+                  | EFocusIn
+                  | EFocusOut
+                  | EKeymapNotify
+                  | EExpose
+                  | EGraphicsExpose
+                  | ENoExpose
+                  | EVisibilityNotify
+                  | ECreateNotify
+                  | EDestroyNotify
+                  | EUnmapNotify
+                  | EMapNotify
+                  | EMapRequest
+                  | EReparentNotify
+                  | EConfigureNotify
+                  | EConfigureRequest
+                  | EGravityNotify
+                  | EResizeRequest
+                  | ECirculateNotify
+                  | ECirculateRequest
+                  | EPropertyNotify
+                  | ESelectionClear
+                  | ESelectionRequest
+                  | ESelectionNotify
+                  | EColormapNotify
+                  | EClientMessage
+                  | EMappingNotify
+                  | ERRScreenChangeNotify
+                  | ERRNotify
+                  | ERRNotifyCrtcChange
+                  | ERRNotifyOutputChange
+                  | ERRNotifyOutputProperty
+                  deriving (Eq, Read, Show)
+
+convEventType :: X.EventType -> Maybe X11EventType
+convEventType e | e == X.keyPress               = Just EKeyPress
+convEventType e | e == X.keyRelease             = Just EKeyRelease
+convEventType e | e == X.buttonPress            = Just EButtonPress
+convEventType e | e == X.buttonRelease          = Just EButtonRelease
+convEventType e | e == X.motionNotify           = Just EMotionNotify
+convEventType e | e == X.enterNotify            = Just EEnterNotify
+convEventType e | e == X.leaveNotify            = Just ELeaveNotify
+convEventType e | e == X.focusIn                = Just EFocusIn
+convEventType e | e == X.focusOut               = Just EFocusOut
+convEventType e | e == X.keymapNotify           = Just EKeymapNotify
+convEventType e | e == X.expose                 = Just EExpose
+convEventType e | e == X.graphicsExpose         = Just EGraphicsExpose
+convEventType e | e == X.noExpose               = Just ENoExpose
+convEventType e | e == X.visibilityNotify       = Just EVisibilityNotify
+convEventType e | e == X.createNotify           = Just ECreateNotify
+convEventType e | e == X.destroyNotify          = Just EDestroyNotify
+convEventType e | e == X.unmapNotify            = Just EUnmapNotify
+convEventType e | e == X.mapNotify              = Just EMapNotify
+convEventType e | e == X.mapRequest             = Just EMapRequest
+convEventType e | e == X.reparentNotify         = Just EReparentNotify
+convEventType e | e == X.configureNotify        = Just EConfigureNotify
+convEventType e | e == X.configureRequest       = Just EConfigureRequest
+convEventType e | e == X.gravityNotify          = Just EGravityNotify
+convEventType e | e == X.resizeRequest          = Just EResizeRequest
+convEventType e | e == X.circulateNotify        = Just ECirculateNotify
+convEventType e | e == X.circulateRequest       = Just ECirculateRequest
+convEventType e | e == X.propertyNotify         = Just EPropertyNotify
+convEventType e | e == X.selectionClear         = Just ESelectionClear
+convEventType e | e == X.selectionRequest       = Just ESelectionRequest
+convEventType e | e == X.selectionNotify        = Just ESelectionNotify
+convEventType e | e == X.colormapNotify         = Just EColormapNotify
+convEventType e | e == X.clientMessage          = Just EClientMessage
+convEventType e | e == X.mappingNotify          = Just EMappingNotify
+convEventType e | e == X.rrScreenChangeNotify   = Just ERRScreenChangeNotify
+convEventType e | e == X.rrNotify               = Just ERRNotify
+convEventType e | e == X.rrNotifyCrtcChange     = Just ERRNotifyCrtcChange
+convEventType e | e == X.rrNotifyOutputChange   = Just ERRNotifyOutputChange
+convEventType e | e == X.rrNotifyOutputProperty = Just ERRNotifyOutputProperty
+convEventType _                                 = Nothing
+
+
+testDrawImage :: IO ()
+testDrawImage = do
+  --edimg <- JP.decodePng <$> BS.readFile file
+  --case edimg of
+  --  Left  err  -> putStrLn err
+  --  Right dimg -> do
+  dpy <- X.openDisplay ""
+  win <- mkUnmanagedWindow dpy 1000 400 500 500
+  let foldMask = foldr (.|.) 0
+  X.selectInput dpy win $ foldMask $ [ X.exposureMask
+                                     , X.keyPressMask
+                                     , X.keyReleaseMask
+                                     , X.buttonPressMask
+                                     , X.buttonReleaseMask
+                                     , X.pointerMotionMask ]
+
+  X.mapWindow dpy win
+  let while m n = m >>= (`when` (n >> while m n))
+  forever $ do drawDia dpy win example1
+               drawDia dpy win example2
+               threadDelay 1000000
+               while ((/= 0) <$> X.pending dpy)
+                 $ X.allocaXEvent
+                 $ \xev -> do X.nextEvent dpy xev
+                              X.get_EventType xev >>= print . convEventType
+               X.pending dpy >>= print
+  --drawDia dpy win $ circle 5 ||| rect 10 8
+  _ <- getLine
+  X.closeDisplay dpy
 
 drawImg :: X.Display -> X.Window -> XImg -> IO ()
 drawImg dpy win ximg = do
@@ -84,7 +201,7 @@ fromJPData img = newArray ((0, 0, 0), (h - 1, w - 1, 3)) 0 >>= setValues
       where
         go :: [(Int, Int, Int)] -> IO XImageArray
         go []             = return arr
-        go ((y, x, c):xs) = do writeArray arr (y, x, c) $ getC c $ getPix x y
+        go ((y, x, c):xs) = do writeArray arr (y, x, c) $! getC c $! getPix x y
                                go xs
 
 makeXImage :: X.Display -> JP.Image JP.PixelRGBA8 -> IO XImg
@@ -149,14 +266,15 @@ mkUnmanagedWindow dpy x y w h = do
   rw <- X.rootWindow dpy (X.defaultScreen dpy)
   let visual = X.defaultVisualOfScreen scr
   let depth  = X.defaultDepthOfScreen scr
-  let attrmask = X.cWOverrideRedirect .|. X.cWBorderPixel .|. X.cWBackPixel
+  --let attrmask = X.cWOverrideRedirect .|. X.cWBorderPixel .|. X.cWBackPixel
   backgroundColor <- initColor dpy "black"
-  borderColor     <- initColor dpy "red"
-  X.allocaSetWindowAttributes $ \attrs -> do
-    X.set_override_redirect attrs True
-    X.set_background_pixel attrs backgroundColor
-    X.set_border_pixel attrs borderColor
-    X.createWindow dpy rw x y w h 1 depth X.inputOutput visual attrmask attrs
+  borderColor     <- initColor dpy "green"
+  --X.allocaSetWindowAttributes $ \attrs -> do
+  --  X.set_override_redirect attrs True
+  --  X.set_background_pixel attrs backgroundColor
+  --  X.set_border_pixel attrs borderColor
+  --  X.createWindow dpy rw x y w h 1 depth X.inputOutput visual attrmask attrs
+  X.createSimpleWindow dpy rw x y w h 1 borderColor backgroundColor
 
 {-
 
