ecb8f8b1938c217a4ac2c382545b2aff29f0d3d9
Remy Goldschmidt <taktoa@gmail.com>
2016-05-01 21:10:12 -0500

    Minor changes to GTKRedo

--- haskell/src/System/HSDM/GTKRedo.hs
+++ haskell/src/System/HSDM/GTKRedo.hs
@@ -5,7 +5,21 @@ module System.HSDM.GTKRedo where
 -- FIXME: remove PackageImport when done
 import           "gtk3" Graphics.UI.Gtk
 
-main = do
+import           Data.Text       (Text)
+import qualified Data.Text       as T
+
+data Login = Login { _username :: Text
+                   , _password :: Text }
+             deriving (Eq, Show, Read)
+
+main :: IO ()
+main = loginScreen (\(Login u p) ->
+                      putStrLn $ mconcat [ "button pressed: "
+                                         , "username = ", show u, ", "
+                                         , "password = ", show p ])
+
+loginScreen :: (Login -> IO ()) -> IO ()
+loginScreen callback = do
   initGUI
 
   builder <- builderNew
@@ -21,9 +35,7 @@ main = do
   on button buttonActivated $ do
     username <- textBufferGetAll userBuf
     password <- textBufferGetAll passBuf
-    putStrLn $ mconcat [ "button pressed: "
-                       , "username = ", username, ", "
-                       , "password = ", password ]
+    callback (Login username password)
 
   on window objectDestroy mainQuit
 
